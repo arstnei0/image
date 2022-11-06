5475d5c663856e5ab6b6c09b117a5756c31b1cae
Zihan Chen <me@zihan.ga>
2022-11-06 03:00:55 +0000

    Create project

---
+++ src/build.js
@@ -0,0 +1,53 @@
+const sharp = require("sharp")
+const glob = require("glob")
+const path = require("path")
+const fs = require("fs-extra")
+
+;(async () => {
+	const chalk = await (await import("chalk")).default
+
+	console.log(chalk.yellow("Clearing output directory..."))
+	fs.emptyDirSync("dist")
+	console.log(chalk.yellow("Output directory cleared."))
+
+	glob("./images/**/*.*", async (err, matches) => {
+		matches.forEach(async (match) => {
+			let imgPath = match.split(path.sep)
+			imgPath.splice(0, 2)
+			imgPath = imgPath.join("/")
+			let outputPath = imgPath.split(".")
+			outputPath.splice(outputPath.length - 1, 1)
+
+			let outputDir = outputPath[0].split(path.sep)
+			outputDir.pop()
+			outputDir = path.join("./dist", outputDir.join("/"))
+
+			outputPath.push("webp")
+			outputPath = path.join("./dist", outputPath.join("."))
+
+			if (!fs.existsSync(outputDir)) {
+				fs.mkdirSync(outputDir, { recursive: true })
+			}
+
+			sharp(match)
+				.webp({
+                    quality: 80
+                })
+				.toBuffer((err, data, info) => {
+					fs.writeFile(
+						outputPath,
+						data,
+						{ flag: "w" },
+						function (err) {
+							if (err) throw err
+							console.log(
+								`${chalk.blue(
+									imgPath
+								)} coonverted to ${chalk.green(outputPath)}`
+							)
+						}
+					)
+				})
+		})
+	})
+})()
