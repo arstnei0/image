b2aa54fc569627796c10c73013247ea5e9bd60eb
Zihan Chen <me@zihan.ga>
2022-11-06 04:44:09 +0000

    Update UI

--- src/ui.js
+++ src/ui.js
@@ -4,12 +4,11 @@ const path = require("path")
 const fs = require("fs-extra")
 
 module.exports = async function generate(p = 'images') {
-    const chalk = await import('chalk')
+    const chalk = await (await import('chalk')).default
     let html = fs.readFileSync('src/template.html').toString()
     let content = ''
 
     glob(p + '/*', (err, matches) => {
-        console.log()
         matches.forEach(match => {
 			let imgPath = match.split(path.sep)
 			imgPath.splice(0, 1)
@@ -18,26 +17,38 @@ module.exports = async function generate(p = 'images') {
 			let outputPath = imgPath.split(".")
 			outputPath.splice(outputPath.length - 1, 1)
 			outputPath.push("webp")
-			outputPath = path.join("./dist", outputPath.join("."))
+			outputPath = outputPath.join(".")
 
             if (/\.(\w*)$/i.test(match)) {
                 content += `<a href="/${outputPath}">${
                     `<p>${imgPath}</p>`
                 }<img src="/${outputPath}" width="100px"></a>\n`
             } else {
+                content += `<a href="/${imgPath}">/${imgPath}</a>\n`
                 generate(match)
             }
         })
 
         html = html.replace('{{}}', content)
 
+        let outputPath = p.split(path.sep)
+        outputPath.splice(0, 1)
+        outputPath = outputPath.join('/')
+
+        const outputDir = path.join('dist', outputPath)
+		if (!fs.existsSync(outputDir)) {
+			fs.mkdirSync(outputDir, { recursive: true })
+		}
+
         fs.writeFileSync(
-            'dist/index.html',
+            path.join('dist', outputPath, 'index.html'),
             html,
             { flag: "w" },
             function (err) {
                 if (err) throw err
             }
         )
+
+        console.log(`${path.join('dist', outputPath, 'index.html')} ${chalk.green('generated.')}`)
     })
 }
